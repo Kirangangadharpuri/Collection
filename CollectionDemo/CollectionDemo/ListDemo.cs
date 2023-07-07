bb1cf88be8d2f9f37422b25fa036b29bfc473906
Kiran <kiranpuri532@gmail.com>
2023-07-07 08:57:34 +0530

    [Kiran] Add:\ perform the all inbuilt methods of list

---
+++ CollectionDemo/CollectionDemo/ListDemo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    //List<T>- class represents the List of object which can accessed with index
+    //It is Dynamic and we can resize the list
+    public class ListDemo
+    {
+        //Creating a list of integers
+        List<int> list = new List<int>();
+        public void Adding()
+        {
+            list.Add(12);
+            list.Add(13);
+            list.Add(14);
+            list.Add(15);
+            list.Add(16);
+            foreach(int elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+            Console.WriteLine("Total elements in list are {0}",list.Count);
+            Console.WriteLine("Total elements in list are {0}", list.Capacity);
+            list.Add(17);
+            list.Add(18);
+            list.Add(19);
+            list.Add(20);
+            foreach (int elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+            Console.WriteLine("Total elements in list are {0}", list.Count);
+            Console.WriteLine("Total elements in list are {0}", list.Capacity);
+            list.RemoveAt(5);
+            Console.WriteLine("#####################");
+            Console.WriteLine("Rmove the element of perticular position");
+            foreach (int elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+            Console.WriteLine("#####################");
+            Console.WriteLine("Print the elements in reverse order");
+
+            list.Reverse();
+            foreach (int elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+            Console.WriteLine("#####################");
+            Console.WriteLine("Print the elements in sorted form");
+
+            list.Sort();
+            foreach (int elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+            Console.WriteLine("#####################");
+            list.Clear();
+            foreach (int elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+}
---
+++ CollectionDemo/CollectionDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    internal class Program
+    {
+        //Collection is a class that means you can create aor declare an object
+        //can be create or declare instance of a class, so we can add a data in collections
+        //collection types- are designed to stare, manage similar data
+        //We can used to adding, removing fine and sorting data in collection
+        //two types of collection- Generic and Non-generic
+        //Non-Generic-ArrayList, HashTable, SortedList, Stanck and Queue
+        //Generic one is List dictionary,SortedtList, Stack and Queue
+        static void Main(string[] args)
+        {
+            ListDemo listDemo = new ListDemo();
+            listDemo.Adding();
+            Console.ReadLine();
+        }
+    }
+}
